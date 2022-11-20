0f632a6001d98fc4838fc6cc42e049a89ac90ab8
Amelia-Bu <56344489+Amelia-Bu@users.noreply.github.com>
2022-11-20 15:51:36 +0800

    Add files via upload

--- Retinex/U-net.py
+++ Retinex/U-net.py
@@ -1,84 +1,96 @@
-# import _init_paths
 import torch
-import torch.nn as nn
-from layers import unetConv2, unetUp
-from utils import init_weights, count_param
-import sys
-import os.path as osp
-def add_path(path):
-    if path not in sys.path:
-        sys.path.insert(0, path)
+from torch import nn
+from torch.nn import functional as F
 
-this_dir = osp.dirname(__file__)
-add_path(osp.join(this_dir, '..'))
-# print(this_dir)
-# print(osp.join(this_dir, '..'))
+class Conv_Block(nn.Module):
+    def __init__(self, in_channel, out_channel):
+        super(Conv_Block, self).__init__()
+        self.layer = nn.Sequential(
+            nn.Conv2d(in_channel, out_channel, 3, 1, 1, padding_mode='reflect',bias=False),
+            nn.BatchNorm2d(out_channel),
+            nn.Dropout2d(0.3),
+            nn.LeakyReLU(),
+            nn.Conv2d(out_channel, out_channel, 3, 1, 1, padding_mode='reflect', bias=False),
+            nn.BatchNorm2d(out_channel),
+            nn.Dropout2d(0.3),
+            nn.LeakyReLU()
+        )
+    def forward(self, x):
+        return self.layer(x)
+
+
+class DownSample(nn.Module):
+    def __init__(self, in_channel, out_channel):
+        super(DownSample, self).__init__()
+        self.layer = nn.Sequential(
+            nn.Conv2d(in_channel, out_channel, 3, 2, 1, padding_mode='reflect', bias=False),
+            nn.Conv2d(out_channel, out_channel, 3, 2, 1, padding_mode='reflect', bias=False),
+            nn.MaxPool2d(kernel_size=3, ceil_mode=True),
+            nn.BatchNorm2d(out_channel),
+            nn.LeakyReLU()
+        )
+    def forward(self,x):
+        return self.layer(x)
+
+
+class UpSample(nn.Module):
+    def __init__(self, in_channel, out_channel):
+        super(UpSample, self).__init__()
+        self.layer = nn.Conv2d(in_channel, out_channel, 3, 2, 1)
+    def forward(self, x, feature_map):
+        up = F.interpolate(x, scale_factor=2, mode='nearest')
+        out = self.layer(up)
+        return torch.cat((out, feature_map), dim=1)
+
+class Fusion(nn.Module):
+
+    def __int__(self, in_channel, out_channel, out):
+        super(Fusion, self).__int__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_channel, out_channel, 3, 2, 1),
+            nn.Conv2d(in_channel, out_channel, 3, 2, 1)
+        )
+
+    def forwar(self,x):
+        return self.layer(x)
+
 
 class UNet(nn.Module):
-
-    def __init__(self, in_channels=1, n_classes=2, feature_scale=2, is_deconv=True, is_batchnorm=True):
+    def __init__(self):
         super(UNet, self).__init__()
-        self.in_channels = in_channels
-        self.feature_scale = feature_scale
-        self.is_deconv = is_deconv
-        self.is_batchnorm = is_batchnorm
+        self.c1 = Conv_Block(3, 32)
+        self.c2 = Conv_Block(32, 32)
+        self.d1 = DownSample(32, 32)
+        self.d2 = DownSample(32, 64)
+        self.d3 = DownSample(64, 128)
+        self.d4 = DownSample(128, 256)
+        self.c3 = Conv_Block(256, 512)
+        self.c4 = Conv_Block(512, 512)
+        self.u1 = UpSample(512, 256)
+        self.f1 = Fusion(384, 256)
+        self.u2 = UpSample(256, 128)
+        self.f2 = Fusion(192, 128)
+        self.u3 = UpSample(128, 64)
+        self.f3 = Fusion(96, 64)
+        self.u4 = UpSample(64, 32)
+        self.f4 = Fusion(64, 32)
+        self.out = nn.Conv2d(32, 3, 3, 1, 1)
 
-        filters = [64, 128, 256, 512, 1024]
-        filters = [int(x / self.feature_scale) for x in filters]
 
-        # downsampling
-        self.maxpool = nn.MaxPool2d(kernel_size=2)
-        self.conv1 = unetConv2(self.in_channels, filters[0], self.is_batchnorm)
-        self.conv2 = unetConv2(filters[0], filters[1], self.is_batchnorm)
-        self.conv3 = unetConv2(filters[1], filters[2], self.is_batchnorm)
-        self.conv4 = unetConv2(filters[2], filters[3], self.is_batchnorm)
-        self.center = unetConv2(filters[3], filters[4], self.is_batchnorm)
-        # upsampling
-        self.up_concat4 = unetUp(filters[4], filters[3], self.is_deconv)
-        self.up_concat3 = unetUp(filters[3], filters[2], self.is_deconv)
-        self.up_concat2 = unetUp(filters[2], filters[1], self.is_deconv)
-        self.up_concat1 = unetUp(filters[1], filters[0], self.is_deconv)
-        # final conv (without any concat)
-        self.final = nn.Conv2d(filters[0], n_classes, 1)
+    def forward(self, x):
+        R1 = self.c1(x)
+        R2 = self.c2(self.d1(R1))
+        R3 = self.c3(self.d2(R2))
+        R4 = self.c4(self.d3(R3))
+        R5 = self.c5(self.d4(R4))
+        O1 = self.c6(self.u1(R5, R4))
+        O2 = self.c7(self.u2(O1, R3))
+        O3 = self.c8(self.u3(O2, R2))
+        O4 = self.c9(self.u4(O3, R1))
 
-        # initialise weights
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                init_weights(m, init_type='kaiming')
-            elif isinstance(m, nn.BatchNorm2d):
-                init_weights(m, init_type='kaiming')
-
-    def forward(self, inputs):
-        conv1 = self.conv1(inputs)  # 16*512*512
-        maxpool1 = self.maxpool(conv1)  # 16*256*256
-
-        conv2 = self.conv2(maxpool1)  # 32*256*256
-        maxpool2 = self.maxpool(conv2)  # 32*128*128
-
-        conv3 = self.conv3(maxpool2)  # 64*128*128
-        maxpool3 = self.maxpool(conv3)  # 64*64*64
-
-        conv4 = self.conv4(maxpool3)  # 128*64*64
-        maxpool4 = self.maxpool(conv4)  # 128*32*32
-
-        center = self.center(maxpool4)  # 256*32*32
-        up4 = self.up_concat4(center, conv4)  # 128*64*64
-        up3 = self.up_concat3(up4, conv3)  # 64*128*128
-        up2 = self.up_concat2(up3, conv2)  # 32*256*256
-        up1 = self.up_concat1(up2, conv1)  # 16*512*512
-
-        final = self.final(up1)
-
-        return final
-
+        return self.out(O4)
 
 if __name__ == '__main__':
-    print('#### Test Case ###')
-    from torch.autograd import Variable
-
-    x = Variable(torch.rand(2, 1, 64, 64)).cuda()
-    model = UNet().cuda()
-    param = count_param(model)
-    y = model(x)
-    print('Output shape:', y.shape)
-    print('UNet totoal parameters: %.2fM (%d)' % (param / 1e6, param))
+    x = torch.randn(2, 3, 256, 256)
+    net = UNet()
+    print(net(x).shape)
